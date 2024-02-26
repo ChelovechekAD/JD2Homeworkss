4ae102c817afba36e21986626edf6e7ff772dba6
ChelovechekAD <yushkevich2004@mail.ru>
2024-02-27 02:17:53 +0300

    hm10

--- Homework10/src/main/java/org/example/DAO/HomeTaskDAO.java
+++
@@ -1,6 +0,0 @@
-package org.example.DAO;
-
-import org.example.Models.HomeTask;
-
-public interface HomeTaskDAO extends DAO<HomeTask>{
-}
--- Homework10/src/main/java/org/example/DAO/WorkTaskDAO.java
+++
@@ -1,6 +0,0 @@
-package org.example.DAO;
-
-import org.example.Models.WorkTask;
-
-public interface WorkTaskDAO extends DAO<WorkTask>{
-}
